fc6d8e3423727ece7220ee967ac362f96b3be549
unknown <gbemilesanmi@gmail.com>
2023-02-11 19:06:40 +0100

    api get req functionnality

--- src/content/pages/Home/Home.js
+++ src/content/pages/Home/Home.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
 import './Home.scss'
 import MovieListing from '../../components/MovieListing/MovieListing'
+import movieApi from '../../../utility/api/MovieApi'
+import {ApiKey} from '../../../utility/api/ApiKey'
 
 const Home = () => {
+  const [search, setSearch] = useState('percy')
+  const [type, setType] = useState('movie')
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const response = await movieApi.get(`?apikey=${ApiKey}&s=${search}&type=${type}`)
+      .catch((err) => { console.log(err.message) })
+      console.log(response)
+    }
+
+    fetchdata()
+  },[])
+  
+
   return (
     <div>
       <div className='banner'></div>
---
+++ src/utility/api/ApiKey.js
@@ -0,0 +1,1 @@
+export const ApiKey = "72b7ba1e"
