18435b1f9aeac4049cdad87426f0c3187ff5f544
unknown <gbemilesanmi@gmail.com>
2023-02-12 01:43:55 +0100

    redux

--- src/content/pages/Home/Home.js
+++ src/content/pages/Home/Home.js
@@ -3,16 +3,20 @@ import './Home.scss'
 import MovieListing from '../../components/MovieListing/MovieListing'
 import movieApi from '../../../utility/api/MovieApi'
 import {ApiKey} from '../../../utility/api/ApiKey'
+import {useDispatch} from 'react-redux'
+import {addMovie} from '../../../features/movies/movieSlice.js'
 
 const Home = () => {
   const [search, setSearch] = useState('percy')
   const [type, setType] = useState('movie')
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const fetchdata = async () => {
       const response = await movieApi.get(`?apikey=${ApiKey}&s=${search}&type=${type}`)
       .catch((err) => { console.log(err.message) })
       console.log(response)
+      dispatch(addMovie(response.data))
     }
 
     fetchdata()
--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import movieReducer from './movies/movieSlice.js'
 
 export const store = configureStore({
-    reducer : {},
+    reducer : movieReducer,
 })
 
 
